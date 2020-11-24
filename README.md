4791141d429560ff81d4d702dbc62afd76dc59f4
Ben Weinstein-Raun <root@benwr.net>
2020-11-24 03:07:10 -0800

    it's a start

---
+++ README.md
@@ -0,0 +1,54 @@
+# Cayley Hashing
+
+This is an implementation of the hash function presented in the paper
+["Navigating in the Cayley Graph of SL₂(𝔽ₚ)"
+](https://link.springer.com/article/10.1007%2Fs00233-015-9766-5) by
+Bromberg, Shpilrain, and Vdovina.
+
+> ## WARNING
+> 
+> This module is not produced by cryptography experts, but by some
+> rando. Furthermore, the algorithm was published in 2017, and is
+> itself not at all battle-tested. Only use this library if you either
+> (a) know what you're doing and have read and understood our code,
+> and/or (b) are building something that does not rely heavily on the
+> cryptographic properties of the hash function.
+>
+> If you _are_ a cryptography expert, we welcome any bug reports or
+> pull requests! We also welcome them if you're not a cryptography
+> expert; this library is quite simple, and should be easy to grok
+> over a coffee with a copy of the paper linked above in hand.
+
+
+# What is this library for?
+
+This library implements a putatively-strong hash function H with the
+following useful property:
+
+For any strings `s1` and `s2`, the hash of their concatenation,
+`H(s1 ++ s2)` can be quickly computed from their respective hashes
+`H(s1)` and `H(s2)`. In this sense it is a "homomorphic hash function".
+
+This property is especially useful for applications where some very
+long string may be constructed via many different routes, but you'd
+nonetheless like to be able to quickly rule out unequal strings.
+
+H has some other useful properties, and is in some limited but
+potentially-useful sense "provably secure". See Bromberg et al. for
+details.
+
+# Technical Details
+
+We use the A(2) and B(2) matrices as generators of SL₂, and
+p = 2^127 - 1 for fast modular arithmetic.
+
+There are not yet any benchmarks, and we have not yet attempted to
+optimize this library at all. However, we needed an
+architecture-agnostic cryptographic hash procedure with a monoid
+homomorphism respecting string concatenation, written in a low-level
+language. While there are [a](https://github.com/srijs/hwsl2-core)
+[few](https://github.com/nspcc-dev/tzhash)
+[implementations](https://github.com/phlegmaticprogrammer/tillich_zemor_hash)
+of related algorithms, e.g. the venerable
+["Hashing with SL₂"](https://link.springer.com/chapter/10.1007/3-540-48658-5_5),
+none of them fulfill the above desiderata.
---
+++ src/lib.rs
@@ -0,0 +1,118 @@
+use std::ops::Mul;
+use std::fmt::Debug;
+
+// big-end first; does this matter?
+type U256 = [u128; 2];
+
+#[derive(PartialEq, Eq, Debug)]
+struct HashMatrix([u128; 4]);
+
+impl Mul for HashMatrix {
+    type Output = Self;
+    fn mul(self, rhs: Self) -> Self {
+        matmul(self, rhs)
+    }
+}
+
+const A: HashMatrix = HashMatrix([
+    1, 2,
+    0, 1,
+]);
+
+const B: HashMatrix = HashMatrix([
+    1, 0,
+    2, 1,
+]);
+
+const P: u128 = (1 << 127) - 1;
+
+// lower 64 bits
+const fn lo_mask(x: u128) -> u128 {
+    x & 0xffff_ffff_ffff_ffff
+}
+
+const fn mul(x: u128, y: u128) -> U256 {
+    // this could probably be made much faster, though I'm not sure.
+    // also I'm not 100% sure it's bug-free; I'm writing it at 1:30am.
+    let xhi = x >> 64;
+    let yhi = y >> 64;
+    let xlo = lo_mask(x);
+    let ylo = lo_mask(y); 
+
+    let xhi_ylo = xhi * ylo;
+    let yhi_xlo = yhi * xlo;
+
+    let (lo_sum_1, carry_bool_1) = (xhi_ylo << 64).overflowing_add(yhi_xlo << 64);
+    let (lo_sum_2, carry_bool_2) = lo_sum_1.overflowing_add(xlo * ylo);
+    let carry = carry_bool_1 as u128 + carry_bool_2 as u128;
+
+    [(xhi * yhi) + (xhi_ylo >> 64) + (yhi_xlo >> 64) + carry, lo_sum_2]
+}
+
+const fn add(x: U256, y: U256) -> U256 {
+    // NOTE: x and y are guaranteed to be <=
+    // (2^127 - 2)^2 = 2^254 - 4 * 2^127 + 4,
+    // so I thinkwe don't have to worry about carries out of here.
+    let (low, carry) = x[1].overflowing_add(y[1]);
+    let high = x[0] + y[0] + carry as u128;
+    [high, low]
+}
+
+const fn mod_p_round(n: U256) -> U256 {
+    let low_bits = n[1] & P; // 127 bits of input
+    let intermediate_bits = (n[0] << 1) | (n[1] >> 127); // 128 of the 129 additional bits
+    let high_bit = n[0] >> 127;
+    let (sum, carry_bool) = low_bits.overflowing_add(intermediate_bits);
+    [carry_bool as u128 + high_bit, sum]
+}
+
+const fn mod_p(n: U256) -> u128 {
+    let n1 = mod_p_round(n); // n1 is at most 130 bits wide
+    let n2 = mod_p_round(n1); // n2 is at most 128 bits wide
+    let n3 = mod_p_round(n2); // n3 is at most 127 bits wide
+
+    ((n3[1] + 1) & P).saturating_sub(1)
+}
+
+const fn matmul(a: HashMatrix, b: HashMatrix) -> HashMatrix {
+    HashMatrix([
+        mod_p(add(mul(a.0[0b00], b.0[0b00]), mul(a.0[0b01], b.0[0b10]))),
+        mod_p(add(mul(a.0[0b00], b.0[0b01]), mul(a.0[0b01], b.0[0b11]))),
+        mod_p(add(mul(a.0[0b10], b.0[0b00]), mul(a.0[0b11], b.0[0b10]))),
+        mod_p(add(mul(a.0[0b10], b.0[0b01]), mul(a.0[0b11], b.0[0b11]))),
+    ])
+}
+
+trait CayleyHashable<T> {
+    fn cayley_hash(t: T) -> HashMatrix;
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn it_works() {
+        assert_eq!(mul(1 << 127, 2), [1,0]);
+        assert_eq!(mul(1 << 127, 1 << 127), [85070591730234615865843651857942052864, 0]);
+        assert_eq!(mul(4, 4), [0, 16]);
+        assert_eq!(mul((1 << 127) + 4, (1 << 127) + 4), [85070591730234615865843651857942052868, 16]);
+
+        assert_eq!(mod_p([0, P]), 0);
+        assert_eq!(mod_p([0, P + 1]), 1);
+        assert_eq!(mod_p([0, 0]), 0);
+        assert_eq!(mod_p([0, 1]), 1);
+        assert_eq!(mod_p([0, P - 1]), P - 1);
+        assert_eq!(mod_p([0, 1 << 127]), 1);
+        assert_eq!(mod_p([1, P]), 2);
+        assert_eq!(mod_p([1, 0]), 2);
+        assert_eq!(mod_p([P, 0]), 0);
+        assert_eq!(mod_p([P, P]), 0);
+        assert_eq!(mod_p([0, u128::MAX]), 1);
+        
+        assert_eq!(HashMatrix([1, 0, 0, 1]) * HashMatrix([1, 0, 0, 1]), HashMatrix([1, 0, 0, 1]));
+        assert_eq!(HashMatrix([2, 0, 0, 2]) * HashMatrix([2, 0, 0, 2]), HashMatrix([4, 0, 0, 4]));
+        assert_eq!(HashMatrix([0, 1, 1, 0]) * HashMatrix([2, 0, 0, 2]), HashMatrix([0, 2, 2, 0]));
+        assert_eq!(HashMatrix([0, 1, 1, 0]) * HashMatrix([2, 0, 0, 2]), HashMatrix([0, 2, 2, 0]));
+    }
+}
