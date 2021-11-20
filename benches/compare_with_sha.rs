70ab1ee4dcd9b9554b45518d64f6a3b48413c567
Basile Henry <bjm.henry@gmail.com>
2021-11-20 12:08:18 +0100

    Add benchmark for hash_par

--- benches/compare_with_sha.rs
+++ benches/compare_with_sha.rs
@@ -7,21 +7,31 @@ use sha3::{Digest, Sha3_512};
 // This isjust stolen straight from the Criterion documentation
 fn from_elem(c: &mut Criterion) {
     static KB: usize = 1024;
+    let sizes = [KB, 2 * KB, 4 * KB, 8 * KB, 16 * KB, 4096 * KB];
 
     let mut group = c.benchmark_group("byte_hashing");
-    for size in [KB, 2 * KB, 4 * KB, 8 * KB, 16 * KB].iter() {
+    for size in sizes.iter() {
         group.throughput(Throughput::Bytes(*size as u64));
         group.bench_with_input(BenchmarkId::new("bromberg", size), size, |b, &size| {
             b.iter(|| black_box(iter::repeat(5u8).take(size).collect::<Vec<u8>>().bromberg_hash()));
         });
     }
-    for size in [KB, 2 * KB, 4 * KB, 8 * KB, 16 * KB].iter() {
+    for size in sizes.iter() {
         group.throughput(Throughput::Bytes(*size as u64));
         group.bench_with_input(BenchmarkId::new("bromberg_strict", size), size, |b, &size| {
             b.iter(|| black_box(hash_strict(&iter::repeat(5u8).take(size).collect::<Vec<u8>>())));
         });
     }
-    for size in [KB, 2 * KB, 4 * KB, 8 * KB, 16 * KB].iter() {
+    #[cfg(feature = "std")]
+    {
+        for size in sizes.iter() {
+            group.throughput(Throughput::Bytes(*size as u64));
+            group.bench_with_input(BenchmarkId::new("bromberg_par", size), size, |b, &size| {
+                b.iter(|| black_box(hash_par(&iter::repeat(5u8).take(size).collect::<Vec<u8>>())));
+            });
+        }
+    }
+    for size in sizes.iter() {
         group.throughput(Throughput::Bytes(*size as u64));
         group.bench_with_input(BenchmarkId::new("sha", size), size, |b, &size| {
             b.iter(|| {
